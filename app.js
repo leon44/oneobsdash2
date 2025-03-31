4eba6f6c29c893b7e207a027ba35466c72954a63
leon.maughan <leon.maughan@dtn.com>
2025-03-31 09:17:03 +0200

    Host Leaflet files locally to avoid mixed content issues

--- app.js
+++ app.js
@@ -121,8 +121,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Create a custom icon for weather stations
     const stationIcon = L.icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconUrl: 'lib/leaflet/marker-icon-2x.png',
+        shadowUrl: 'lib/leaflet/marker-shadow.png',
         iconSize: [25, 41],
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
